cb0bc278f0098b1ab8b9074a0b25893e996694fd
justineclaire <langridge.justine@gmail.com>
2022-03-05 21:03:04 +0000

    hula hoop

--- java/src/example/RotatingAudioBands.java
+++ java/src/example/RotatingAudioBands.java
@@ -32,7 +32,7 @@ public class RotatingAudioBands extends Visual {
         setFrameSize(256);
 
         startMinim();
-        loadAudio("heroplanet.mp3");
+        loadAudio("hula-hoops.mp3");
         getAudioPlayer().play();
         //startListening(); 
         
