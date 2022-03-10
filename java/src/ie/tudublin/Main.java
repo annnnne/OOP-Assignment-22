98c613e82f84222ebefd52238f6e84702de9a944
justineclaire <langridge.justine@gmail.com>
2022-03-10 20:27:26 +0000

    testmena

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,8 +1,11 @@
 package ie.tudublin;
 
+import example.AudioBandsVisual;
 import example.CubeVisual;
+import example.CubeVisual1;
 import example.MyVisual;
 import example.RotatingAudioBands;
+import example.WaveForm;
 
 public class Main
 {	
@@ -10,7 +13,7 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyVisual());		
+        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
 	}
 
 	public static void main(String[] args)
